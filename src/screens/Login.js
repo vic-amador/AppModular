e622c195a7233b9fc460790e9fca500cf63e8a2c
Patrick <polay2patricio@gmail.com>
2023-05-25 01:44:33 -0600

    agregar screen my info

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -32,6 +32,7 @@ const Login = ({navigation}) => {
             fontFamily: 'Louis-George-Cafe',
             fontSize: 40,
             marginBottom: 30,
+            marginLeft: 55,
           }}>
           Iniciar Sesión
         </Text>
--- src/screens/MyInfo.js
+++ src/screens/MyInfo.js
@@ -1,14 +1,76 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { 
+  Text,
+  View, 
+  ImageBackground, 
+  TouchableOpacity,
+} from 'react-native';
+import React, { Component } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
-export class MyInfo extends Component {
-  render() {
+/*#export class MyInfo extends Component {*/
+  const MyInfo = ({navigation}) => {
     return (
       <View>
-        <Text>MyInfo</Text>
+        <ImageBackground
+            source={require('../assets/Images/userDef.png')}
+            style={{width: 120, height: 120, marginLeft: 130, marginTop: 75}}
+            imageStyle={{borderRadius: 40}}
+          />
+        <Text
+          style={{
+            fontFamily: 'Louis-George-Cafe',
+            fontSize: 20,
+            marginTop: 20,
+            marginBottom: 15,
+            marginLeft: 20, 
+          }}>
+          Nombre: </Text>
+        <Text
+          style={{
+            fontFamily: 'Louis-George-Cafe',
+            fontSize: 20,
+            marginTop: 10,
+            marginBottom: 15,
+            marginLeft: 20, 
+          }}>
+          Correo: </Text>
+          <Text
+            style={{
+              fontFamily: 'Louis-George-Cafe',
+              fontSize: 20,
+              marginTop: 10,
+              marginBottom: 30,
+              marginLeft: 20, 
+            }}>
+          Cantidad de reportes: </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Inicio')}
+            style={{
+              backgroundColor: '#ad40af',
+              padding: 10,
+              width: '34%',
+              borderRadius: 5,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 120,
+              marginLeft: 120,
+            }}>
+            <Text
+              style={{
+                fontFamily: 'Louis-George-Cafe',
+                fontSize: 20,
+                color: 'white',
+              }}>
+              Inicio
+            </Text>
+          </TouchableOpacity>
       </View>
+      
     )
   }
-}
+/*}*/
 
 export default MyInfo
+
